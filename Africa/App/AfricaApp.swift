df0968086d553b4b3579a1f07e15769147d3a883
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-07-03 14:28:34 -0500

    First Commit

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Oscar David Myerston Vega on 3/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Africa/App/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Oscar David Myerston Vega on 3/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+        Text("Contents")
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
